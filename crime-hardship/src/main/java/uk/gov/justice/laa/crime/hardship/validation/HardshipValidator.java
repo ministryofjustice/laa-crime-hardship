625fd6509aa2c97e9aefd682065ef649a1b2f015
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2023-08-11 10:18:02 +0100

    added unit tests and renamed vars based on review comment

--- crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/validation/HardshipValidator.java
+++ crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/validation/HardshipValidator.java
@@ -1,26 +1,25 @@
 package uk.gov.justice.laa.crime.hardship.validation;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import uk.gov.justice.laa.crime.hardship.exception.ValidationException;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
-@Slf4j
 @Component
 public class HardshipValidator {
 
-    private Optional<Void> checkReviewDate(LocalDate reviewDate, LocalDate initAssDate) {
-        if (reviewDate.isBefore(initAssDate)) {
+    protected Optional<Void> checkReviewDate(LocalDate reviewDate, LocalDate initialAssessmentDate) {
+        if (reviewDate.isBefore(initialAssessmentDate)) {
             throw new ValidationException("Hardship review date precedes the initial assessment date");
         }
 
         return Optional.empty();
     }
 
-    private Optional<Void> checkReviewDate(LocalDate reviewDate, LocalDate initAssDate, LocalDate fullAssDate) {
-        if (reviewDate.isBefore(fullAssDate) || reviewDate.isBefore(initAssDate)) {
+    protected Optional<Void> checkReviewDate(LocalDate reviewDate, LocalDate initialAssessmentDate,
+                                             LocalDate fullAssessmentDate) {
+        if (reviewDate.isBefore(fullAssessmentDate) || reviewDate.isBefore(initialAssessmentDate)) {
             throw new ValidationException("Hardship review date precedes the initial or full assessment dates");
         }
 
