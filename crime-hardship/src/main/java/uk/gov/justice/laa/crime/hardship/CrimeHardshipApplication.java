9dc8b0c2841b81f4d3d1f9af0d034b2b11b30f10
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2023-08-15 14:02:09 +0100

    added scan to pick missing config properties causing the error running the app

--- crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/CrimeHardshipApplication.java
+++ crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/CrimeHardshipApplication.java
@@ -2,8 +2,10 @@ package uk.gov.justice.laa.crime.hardship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CrimeHardshipApplication {
 
 	public static void main(String[] args) {
