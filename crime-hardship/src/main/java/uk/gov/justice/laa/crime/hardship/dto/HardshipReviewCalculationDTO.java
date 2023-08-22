a917b5368e86750b71fe75eb01a7a687f2d04753
Aafreen Ansari <aafreen.ansari@digital.justice.gov.uk>
2023-08-22 14:01:13 +0100

    LCAM-617| added default for disposableIncome and constructor annotations

--- crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/dto/HardshipReviewCalculationDTO.java
+++ crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/dto/HardshipReviewCalculationDTO.java
@@ -1,15 +1,20 @@
 package uk.gov.justice.laa.crime.hardship.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HardshipReviewCalculationDTO {
 
     private List<HardshipReviewCalculationDetail> hardshipReviewCalculationDetails;
-    private BigDecimal disposableIncome;
+    @Builder.Default
+    private BigDecimal disposableIncome = BigDecimal.ZERO;
 }
--- crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/dto/HardshipReviewCalculationDetail.java
+++ crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/dto/HardshipReviewCalculationDetail.java
@@ -1,7 +1,9 @@
 package uk.gov.justice.laa.crime.hardship.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import uk.gov.justice.laa.crime.hardship.staticdata.enums.Frequency;
 import uk.gov.justice.laa.crime.hardship.staticdata.enums.HardshipReviewDetailType;
 
@@ -9,6 +11,8 @@ import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HardshipReviewCalculationDetail {
 
     private HardshipReviewDetailType detailType;
