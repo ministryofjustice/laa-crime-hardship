33bac15307db8f49dc5676f8bdb827c00bef3761
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-09-06 13:14:42 +0100

    Test scenarios for PersistHardshipMapper

--- crime-hardship/src/test/java/uk/gov/justice/laa/crime/hardship/data/builder/TestModelDataBuilder.java
+++ crime-hardship/src/test/java/uk/gov/justice/laa/crime/hardship/data/builder/TestModelDataBuilder.java
@@ -1,30 +1,39 @@
 package uk.gov.justice.laa.crime.hardship.data.builder;
 
 import org.springframework.stereotype.Component;
+import uk.gov.justice.laa.crime.hardship.dto.HardshipResult;
 import uk.gov.justice.laa.crime.hardship.dto.HardshipReviewCalculationDTO;
 import uk.gov.justice.laa.crime.hardship.dto.HardshipReviewCalculationDetail;
 import uk.gov.justice.laa.crime.hardship.dto.maat_api.HardshipReviewDetail;
-import uk.gov.justice.laa.crime.hardship.model.ApiCalculateHardshipByDetailRequest;
-import uk.gov.justice.laa.crime.hardship.model.ApiCalculateHardshipByDetailResponse;
-import uk.gov.justice.laa.crime.hardship.staticdata.enums.Frequency;
-import uk.gov.justice.laa.crime.hardship.staticdata.enums.HardshipReviewDetailType;
+import uk.gov.justice.laa.crime.hardship.model.*;
+import uk.gov.justice.laa.crime.hardship.staticdata.enums.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 import static java.util.stream.Collectors.toList;
 
 @Component
 public class TestModelDataBuilder {
 
-    public static final Integer TEST_REP_ID = 91919;
-    public static final String MEANS_ASSESSMENT_TRANSACTION_ID = "7c49ebfe-fe3a-4f2f-8dad-f7b8f03b8327";
-    public static final String DETAIL_TYPE = "EXPENDITURE";
-    public static final BigDecimal HARDSHIP_SUMMARY = BigDecimal.valueOf(100.12);
+    public static final Integer CMU_ID = 50;
     public static final Integer HARDSHIP_ID = 1234;
-    public static final BigDecimal HARDSHIP_AMOUNT = BigDecimal.valueOf(10.0);
+    public static final Integer TEST_REP_ID = 91919;
+    public static final String TEST_USER_NAME = "mock-u";
+    public static final String DETAIL_TYPE = "EXPENDITURE";
+    public static final Integer FINANCIAL_ASSESSMENT_ID = 6781;
     public static final BigDecimal FULL_THRESHOLD = BigDecimal.valueOf(3000.0);
+    public static final BigDecimal HARDSHIP_AMOUNT = BigDecimal.valueOf(10.0);
+    public static final BigDecimal HARDSHIP_SUMMARY = BigDecimal.valueOf(100.12);
+    public static final BigDecimal TOTAL_DISPOSABLE_INCOME = BigDecimal.valueOf(500);
+    public static final BigDecimal POST_HARDSHIP_DISPOSABLE_INCOME = BigDecimal.valueOf(250);
+    public static final String MEANS_ASSESSMENT_TRANSACTION_ID = "7c49ebfe-fe3a-4f2f-8dad-f7b8f03b8327";
+    public static final LocalDateTime RESULT_DATE = LocalDateTime.of(2022, 12, 14, 0, 0, 0);
 
     public static ApiCalculateHardshipByDetailRequest getApiCalculateHardshipByDetailRequest(boolean isValid) {
         return new ApiCalculateHardshipByDetailRequest()
@@ -36,6 +45,89 @@ public class TestModelDataBuilder {
     public static ApiCalculateHardshipByDetailResponse getApiCalculateHardshipByDetailResponse() {
         return new ApiCalculateHardshipByDetailResponse()
                 .withHardshipSummary(HARDSHIP_SUMMARY);
+    }
+
+    public static HardshipReview getMinimalHardshipReview() {
+        return new HardshipReview()
+                .withReviewReason(NewWorkReason.PRI)
+                .withCmuId(CMU_ID)
+                .withReviewDate(LocalDateTime.now())
+                .withHardshipReviewId(HARDSHIP_ID)
+                .withNotes("Mock Note.")
+                .withDecisionNotes("Mock Decision Note.")
+                .withSolicitorCosts(TestModelDataBuilder.getSolicitorsCosts())
+                .withTotalAnnualDisposableIncome(TOTAL_DISPOSABLE_INCOME)
+                .withReviewStatus(HardshipReviewStatus.COMPLETE)
+
+                .withRepId(TestModelDataBuilder.TEST_REP_ID)
+                .withUserSession(TestModelDataBuilder.getUserSession())
+                .withCourtType(CourtType.MAGISTRATE)
+                .withFinancialAssessmentId(TestModelDataBuilder.FINANCIAL_ASSESSMENT_ID);
+    }
+
+    public static HardshipResult getHardshipResult() {
+        return HardshipResult.builder()
+                .resultDate(RESULT_DATE)
+                .result(HardshipReviewResult.PASS)
+                .postHardshipDisposableIncome(POST_HARDSHIP_DISPOSABLE_INCOME)
+                .build();
+    }
+
+    public static SolicitorCosts getSolicitorsCosts() {
+        return new SolicitorCosts()
+                .withVat(BigDecimal.TEN)
+                .withDisbursements(BigDecimal.ZERO)
+                .withRate(BigDecimal.TEN)
+                .withHours(50)
+                .withEstimatedTotal(BigDecimal.valueOf(2000));
+    }
+
+    public static DeniedIncome getDeniedIncome() {
+        return new DeniedIncome()
+                .withAccepted(true)
+                .withAmount(BigDecimal.TEN)
+                .withFrequency(Frequency.MONTHLY)
+                .withType(HardshipReviewDetailType.INCOME)
+                .withReasonNote("Hospitalisation")
+                .withItemCode(DeniedIncomeDetailCode.MEDICAL_GROUNDS);
+    }
+
+    public static ExtraExpenditure getExtraExpenditure() {
+        return new ExtraExpenditure()
+                .withAccepted(true)
+                .withAmount(BigDecimal.TEN)
+                .withFrequency(Frequency.TWO_WEEKLY)
+                .withType(HardshipReviewDetailType.EXPENDITURE)
+                .withReasonCode(HardshipReviewDetailReasons.ESSENTIAL_ITEM)
+                .withItemCode(ExtraExpenditureDetailCode.CARDS);
+    }
+
+    public static OtherFundingSource getOtherFundingSources() {
+        return new OtherFundingSource()
+                .withType(HardshipReviewDetailType.FUNDING)
+                .withAmount(BigDecimal.ONE)
+                .withDueDate(LocalDateTime.MAX)
+                .withDescription("Loan from parents");
+    }
+
+    public static HardshipProgress getHardshipProgress() {
+        return new HardshipProgress()
+                .withDateTaken(LocalDateTime.of(
+                        LocalDate.ofYearDay(2022, 235), LocalTime.NOON))
+                .withDateCompleted(LocalDateTime.of(
+                        LocalDate.ofYearDay(2022, 250), LocalTime.NOON)
+                )
+                .withDateRequired(LocalDateTime.of(
+                        LocalDate.ofYearDay(2022, 300), LocalTime.NOON)
+                )
+                .withAction(HardshipReviewProgressAction.ADDITIONAL_EVIDENCE)
+                .withResponse(HardshipReviewProgressResponse.ADDITIONAL_PROVIDED);
+    }
+
+    public static ApiUserSession getUserSession() {
+        return new ApiUserSession()
+                .withUserName(TEST_USER_NAME)
+                .withSessionId(UUID.randomUUID().toString());
     }
 
     public static List<HardshipReviewDetail> getHardshipReviewDetailList(String accepted, double amount) {
---
+++ crime-hardship/src/test/java/uk/gov/justice/laa/crime/hardship/mapper/PersistHardshipMapperTest.java
@@ -0,0 +1,199 @@
+package uk.gov.justice.laa.crime.hardship.mapper;
+
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import uk.gov.justice.laa.crime.hardship.data.builder.TestModelDataBuilder;
+import uk.gov.justice.laa.crime.hardship.dto.HardshipResult;
+import uk.gov.justice.laa.crime.hardship.dto.HardshipReviewDTO;
+import uk.gov.justice.laa.crime.hardship.model.*;
+import uk.gov.justice.laa.crime.hardship.model.maat_api.*;
+import uk.gov.justice.laa.crime.hardship.staticdata.enums.HardshipReviewDetailCode;
+import uk.gov.justice.laa.crime.hardship.staticdata.enums.HardshipReviewDetailType;
+import uk.gov.justice.laa.crime.hardship.staticdata.enums.RequestType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SoftAssertionsExtension.class)
+class PersistHardshipMapperTest {
+
+
+    HardshipReviewDTO reviewDTO;
+    PersistHardshipMapper mapper = new PersistHardshipMapper();
+
+    @InjectSoftAssertions
+    private SoftAssertions softly;
+
+    @BeforeEach
+    void setup() {
+        this.reviewDTO = HardshipReviewDTO.builder()
+                .hardship(TestModelDataBuilder.getMinimalHardshipReview())
+                .hardshipResult(TestModelDataBuilder.getHardshipResult())
+                .build();
+    }
+
+    @Test
+    void givenMinimalHardshipReviewDTO_whenFromDtoIsInvoked_thenRequestIsMapped() {
+
+        HardshipReview hardship = reviewDTO.getHardship();
+        HardshipResult result = reviewDTO.getHardshipResult();
+
+        ApiPersistHardshipRequest request = mapper.fromDto(reviewDTO);
+
+        softly.assertThat(request.getNworCode())
+                .isEqualTo(hardship.getReviewReason().getCode());
+        softly.assertThat(request.getCmuId())
+                .isEqualTo(hardship.getCmuId());
+        softly.assertThat(request.getReviewResult())
+                .isEqualTo(result.getResult());
+        softly.assertThat(request.getReviewDate())
+                .isEqualTo(hardship.getReviewDate());
+        softly.assertThat(request.getResultDate().toLocalDate())
+                .isEqualTo(LocalDateTime.now().toLocalDate());
+        softly.assertThat(request.getNotes())
+                .isEqualTo(hardship.getNotes());
+        softly.assertThat(request.getDecisionNotes())
+                .isEqualTo(hardship.getDecisionNotes());
+        softly.assertThat(request.getStatus())
+                .isEqualTo(hardship.getReviewStatus());
+        softly.assertThat(request.getDisposableIncome())
+                .isEqualTo(hardship.getTotalAnnualDisposableIncome());
+        softly.assertThat(request.getDisposableIncomeAfterHardship())
+                .isEqualTo(result.getPostHardshipDisposableIncome());
+
+        var solicitorsCosts = request.getSolicitorCosts();
+        softly.assertThat(solicitorsCosts.getSolicitorDisb())
+                .isEqualTo(hardship.getSolicitorCosts().getDisbursements());
+        softly.assertThat(solicitorsCosts.getSolicitorRate())
+                .isEqualTo(hardship.getSolicitorCosts().getRate());
+        softly.assertThat(solicitorsCosts.getSolicitorVat())
+                .isEqualTo(hardship.getSolicitorCosts().getVat());
+        softly.assertThat(solicitorsCosts.getSolicitorHours())
+                .isEqualTo(hardship.getSolicitorCosts().getHours());
+        softly.assertThat(solicitorsCosts.getSolicitorEstTotalCost())
+                .isEqualTo(hardship.getSolicitorCosts().getEstimatedTotal());
+    }
+
+    @Test
+    void givenHardshipReviewDTOWithDetails_whenFromDtoIsInvoked_thenRequestIsMapped() {
+        HardshipReview hardship = reviewDTO.getHardship();
+
+        DeniedIncome deniedIncome = TestModelDataBuilder.getDeniedIncome();
+        ExtraExpenditure extraExpenditure = TestModelDataBuilder.getExtraExpenditure();
+        OtherFundingSource otherFundingSource = TestModelDataBuilder.getOtherFundingSources();
+
+        hardship.setDeniedIncome(List.of(deniedIncome));
+        hardship.setExtraExpenditure(List.of(extraExpenditure));
+        hardship.setOtherFundingSources(List.of(otherFundingSource));
+
+        ApiPersistHardshipRequest request = mapper.fromDto(reviewDTO);
+        List<ApiHardshipDetail> reviewDetails = request.getReviewDetails();
+
+        assertThat(reviewDetails)
+                .asList()
+                .hasSize(3);
+
+        List<ApiHardshipDetail> expected = List.of(
+
+                new ApiHardshipDetail()
+                        .withAccepted(true)
+                        .withType(HardshipReviewDetailType.INCOME)
+                        .withAmount(deniedIncome.getAmount())
+                        .withFrequency(deniedIncome.getFrequency())
+                        .withUserCreated(hardship.getUserSession().getUserName())
+                        .withDetailCode(HardshipReviewDetailCode.MEDICAL_GROUNDS),
+
+                new ApiHardshipDetail()
+                        .withAccepted(true)
+                        .withType(HardshipReviewDetailType.EXPENDITURE)
+                        .withAmount(extraExpenditure.getAmount())
+                        .withFrequency(extraExpenditure.getFrequency())
+                        .withUserCreated(hardship.getUserSession().getUserName())
+                        .withDetailCode(HardshipReviewDetailCode.CARDS)
+                        .withDetailReason(extraExpenditure.getReasonCode()),
+
+                new ApiHardshipDetail()
+                        .withType(HardshipReviewDetailType.FUNDING)
+                        .withAmount(otherFundingSource.getAmount())
+                        .withDateDue(otherFundingSource.getDueDate())
+                        .withUserCreated(hardship.getUserSession().getUserName())
+                        .withOtherDescription(otherFundingSource.getDescription())
+        );
+
+        softly.assertThat(reviewDetails)
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsAll(expected);
+    }
+
+    @Test
+    void givenHardshipReviewDTOWithProgress_whenFromDtoIsInvoked_thenRequestIsMapped() {
+        HardshipReview hardship = reviewDTO.getHardship();
+        HardshipProgress progress = TestModelDataBuilder.getHardshipProgress();
+        hardship.setProgressItems(List.of(progress));
+
+        ApiPersistHardshipRequest request = mapper.fromDto(reviewDTO);
+
+        List<ApiHardshipProgress> expected = List.of(
+                new ApiHardshipProgress()
+                        .withProgressAction(progress.getAction())
+                        .withProgressResponse(progress.getResponse())
+                        .withDateRequested(progress.getDateTaken())
+                        .withDateCompleted(progress.getDateCompleted())
+                        .withDateRequired(progress.getDateRequired())
+                        .withUserCreated(hardship.getUserSession().getUserName())
+        );
+
+        assertThat(request.getReviewProgressItems())
+                .asList()
+                .usingRecursiveFieldByFieldElementComparator()
+                .containsAll(expected);
+
+    }
+
+    @Test
+    void givenHardshipReviewDTOAndCreateRequest_whenFromDtoIsInvoked_thenRequestIsMapped() {
+        HardshipReview hardship = reviewDTO.getHardship();
+        reviewDTO.setRequestType(RequestType.CREATE);
+
+        ApiCreateHardshipRequest request = (ApiCreateHardshipRequest) mapper.fromDto(reviewDTO);
+
+        softly.assertThat(request.getRepId())
+                .isEqualTo(hardship.getRepId());
+        softly.assertThat(request.getUserCreated())
+                .isEqualTo(hardship.getUserSession().getUserName());
+        softly.assertThat(request.getCourtType())
+                .isEqualTo(hardship.getCourtType());
+        softly.assertThat(request.getFinancialAssessmentId())
+                .isEqualTo(hardship.getFinancialAssessmentId());
+    }
+
+    @Test
+    void givenHardshipReviewDTOAndUpdateRequest_whenFromDtoIsInvoked_thenRequestIsMapped() {
+        HardshipReview hardship = reviewDTO.getHardship();
+        reviewDTO.setRequestType(RequestType.UPDATE);
+
+        ApiUpdateHardshipRequest request = (ApiUpdateHardshipRequest) mapper.fromDto(reviewDTO);
+
+        softly.assertThat(request.getId())
+                .isEqualTo(hardship.getHardshipReviewId());
+        softly.assertThat(request.getUserModified())
+                .isEqualTo(hardship.getUserSession().getUserName());
+    }
+
+    @Test
+    void givenApiPersistHardshipResponse_whenToDtoIsInvoked_thenDtoIsMapped() {
+        ApiPersistHardshipResponse response = new ApiPersistHardshipResponse()
+                .withId(TestModelDataBuilder.HARDSHIP_ID);
+
+        mapper.toDto(response, reviewDTO);
+
+        assertThat(reviewDTO.getHardship().getHardshipReviewId())
+                .isEqualTo(response.getId());
+    }
+}
