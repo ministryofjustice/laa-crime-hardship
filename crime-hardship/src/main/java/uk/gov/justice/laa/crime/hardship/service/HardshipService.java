04acd26779ebcd04051501e86313af6b072d5189
Aafreen Ansari <aafreen.ansari@digital.justice.gov.uk>
2023-10-09 11:44:22 +0100

    LCAM-927-added rollback endpoint, service and test

--- crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/service/HardshipService.java
+++ crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/service/HardshipService.java
@@ -9,9 +9,12 @@ import uk.gov.justice.laa.crime.hardship.mapper.PersistHardshipMapper;
 import uk.gov.justice.laa.crime.hardship.model.HardshipReview;
 import uk.gov.justice.laa.crime.hardship.model.maat_api.ApiPersistHardshipRequest;
 import uk.gov.justice.laa.crime.hardship.model.maat_api.ApiPersistHardshipResponse;
+import uk.gov.justice.laa.crime.hardship.staticdata.enums.HardshipReviewStatus;
 import uk.gov.justice.laa.crime.hardship.staticdata.enums.RequestType;
 
 import java.math.BigDecimal;
+
+import static java.util.Optional.ofNullable;
 
 @Slf4j
 @Service
@@ -28,6 +31,21 @@ public class HardshipService {
 
     public HardshipReviewDTO update(HardshipReviewDTO hardshipReviewDTO, String laaTransactionId) {
         return persist(hardshipReviewDTO, laaTransactionId, RequestType.UPDATE);
+    }
+
+    public HardshipReviewDTO rollback(HardshipReviewDTO hardshipReviewDTO, String laaTransactionId) {
+
+        hardshipReviewDTO.getHardshipMetadata().setReviewStatus(HardshipReviewStatus.IN_PROGRESS);
+
+        ofNullable(hardshipReviewDTO.getHardshipResult())
+                .orElse(new HardshipResult())
+                .setResult(null);
+
+        ApiPersistHardshipRequest request = mapper.fromDto(hardshipReviewDTO);
+        ApiPersistHardshipResponse response =
+                maatCourtDataService.persistHardship(request, laaTransactionId, RequestType.UPDATE);
+        mapper.toDto(response, hardshipReviewDTO);
+        return hardshipReviewDTO;
     }
 
     private HardshipReviewDTO persist(HardshipReviewDTO hardshipReviewDTO, String laaTransactionId, RequestType requestType) {
