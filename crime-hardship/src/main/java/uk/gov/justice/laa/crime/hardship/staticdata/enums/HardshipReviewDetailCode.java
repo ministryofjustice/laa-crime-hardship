b42c2fed525102169114c9f8e3078dc2dce06dd7
Nigel Pain <nigel.pain@digital.justice.gov.uk>
2023-10-26 12:06:30 +0100

    refactored enums to remove type and add missing value

--- crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/staticdata/enums/HardshipReviewDetailCode.java
+++ crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/staticdata/enums/HardshipReviewDetailCode.java
@@ -13,33 +13,32 @@ import java.util.stream.Stream;
 @Getter
 @AllArgsConstructor
 public enum HardshipReviewDetailCode {
-    UNSECURED_LOAN("UNSECURED LOAN", "Unsecured Loan", HardshipReviewDetailType.EXPENDITURE.getType()),
-    SECURED_LOAN("SECURED LOAN", "Secured Loan", HardshipReviewDetailType.EXPENDITURE.getType()),
-    CAR_LOAN("CAR LOAN", "Car Loan", HardshipReviewDetailType.EXPENDITURE.getType()),
-    IVA("IVA", "IVA", HardshipReviewDetailType.EXPENDITURE.getType()),
-    CARDS("CARDS", "Credit/Store Card Payment", HardshipReviewDetailType.EXPENDITURE.getType()),
-    DEBTS("DEBTS", "Debts", HardshipReviewDetailType.EXPENDITURE.getType()),
-    FINES("FINES", "Fines", HardshipReviewDetailType.EXPENDITURE.getType()),
-    RENT_ARREARS("RENT ARREARS", "Rent Arrears", HardshipReviewDetailType.EXPENDITURE.getType()),
-    BAILIFF("BAILIFF", "Bailiff Costs", HardshipReviewDetailType.EXPENDITURE.getType()),
-    DWP_OVERPAYMENT("DWP OVERPAYMENT", "DWP Overpayment", HardshipReviewDetailType.EXPENDITURE.getType()),
-    STUDENT_LOAN("STUDENT LOAN", "Student Loan", HardshipReviewDetailType.EXPENDITURE.getType()),
-    ADD_MORTGAGE("ADD MORTGAGE", "Mortgage on additional Property", HardshipReviewDetailType.EXPENDITURE.getType()),
-    UNI_HOUSING("UNI HOUSING", "University Housing Costs", HardshipReviewDetailType.EXPENDITURE.getType()),
-    PRESCRIPTION("PRESCRIPTION", "Prescription Costs", HardshipReviewDetailType.EXPENDITURE.getType()),
-    PENSION_PAY("PENSION PAY", "Pension Payments", HardshipReviewDetailType.EXPENDITURE.getType()),
+    UNSECURED_LOAN("UNSECURED LOAN", "Unsecured Loan"),
+    SECURED_LOAN("SECURED LOAN", "Secured Loan"),
+    CAR_LOAN("CAR LOAN", "Car Loan"),
+    IVA("IVA", "IVA"),
+    CARDS("CARDS", "Credit/Store Card Payment"),
+    DEBTS("DEBTS", "Debts"),
+    FINES("FINES", "Fines"),
+    RENT_ARREARS("RENT ARREARS", "Rent Arrears"),
+    BAILIFF("BAILIFF", "Bailiff Costs"),
+    DWP_OVERPAYMENT("DWP OVERPAYMENT", "DWP Overpayment"),
+    STUDENT_LOAN("STUDENT LOAN", "Student Loan"),
+    ADD_MORTGAGE("ADD MORTGAGE", "Mortgage on additional Property"),
+    UNI_HOUSING("UNI HOUSING", "University Housing Costs"),
+    PRESCRIPTION("PRESCRIPTION", "Prescription Costs"),
+    PENSION_PAY("PENSION PAY", "Pension Payments"),
     // Typo in the data that is already in the table
-    MEDICAL_COSTS("MEDIAL COSTS", "Medical Costs", HardshipReviewDetailType.EXPENDITURE.getType()),
-    OTHER("OTHER", "Other", HardshipReviewDetailType.EXPENDITURE.getType()),
-    MEDICAL_GROUNDS("MEDICAL GROUNDS", "Medical Grounds", HardshipReviewDetailType.INCOME.getType()),
-    SUSPENDED_WORK("SUSPENDED WORK", "Suspended from work", HardshipReviewDetailType.INCOME.getType()),
-    OTHER_INC("OTHER INC", "Other", HardshipReviewDetailType.INCOME.getType());
+    MEDICAL_COSTS("MEDIAL COSTS", "Medical Costs"),
+    OTHER("OTHER", "Other"),
+    MEDICAL_GROUNDS("MEDICAL GROUNDS", "Medical Grounds"),
+    SUSPENDED_WORK("SUSPENDED WORK", "Suspended from work"),
+    OTHER_INC("OTHER INC", "Other");
 
     @JsonPropertyDescription("Hardship review detail codes that are valid")
     @JsonValue
     private final String code;
     private final String description;
-    private final String type;
 
     public static HardshipReviewDetailCode getFrom(String code) {
         if (StringUtils.isBlank(code)) { return null; }
