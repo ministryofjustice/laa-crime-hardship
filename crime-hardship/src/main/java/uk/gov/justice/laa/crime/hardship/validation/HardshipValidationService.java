b25ac041b966f8771f5fb99ef9decf94482f399d
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2024-01-04 15:51:36 +0000

    New check on updates & improved constant identifiers
    This will ensure that completed hardships cannot be modified

--- crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/validation/HardshipValidationService.java
+++ crime-hardship/src/main/java/uk/gov/justice/laa/crime/hardship/validation/HardshipValidationService.java
@@ -6,12 +6,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.laa.crime.hardship.dto.maat_api.FinancialAssessmentDTO;
 import uk.gov.justice.laa.crime.hardship.exception.ValidationException;
-import uk.gov.justice.laa.crime.hardship.model.ApiPerformHardshipRequest;
-import uk.gov.justice.laa.crime.hardship.model.DeniedIncome;
-import uk.gov.justice.laa.crime.hardship.model.ExtraExpenditure;
-import uk.gov.justice.laa.crime.hardship.model.HardshipProgress;
+import uk.gov.justice.laa.crime.hardship.model.*;
 import uk.gov.justice.laa.crime.hardship.service.MaatCourtDataService;
 import uk.gov.justice.laa.crime.hardship.staticdata.enums.HardshipReviewStatus;
+import uk.gov.justice.laa.crime.hardship.staticdata.enums.RequestType;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -27,18 +25,20 @@ public class HardshipValidationService {
 
     private final MaatCourtDataService maatCourtDataService;
 
-    public static final String INCOMPLETE_ASSESSMENT_VALIDATION_MESSAGE =
+    public static final String CANNOT_MODIFY_COMPLETE_HARDSHIP_ERROR =
+            "Cannot modify a complete hardship review";
+    public static final String INCOMPLETE_ASSESSMENT_ERROR =
             "Hardship review can only be entered after a completed assessment";
-    public static final String REVIEW_DATE_VALIDATION_MESSAGE =
+    public static final String REVIEW_DATE_ERROR =
             "Hardship review date precedes the initial or full assessment date(s)";
-    public static final String REVIEW_STATUS_VALIDATION_MESSAGE =
+    public static final String REVIEW_STATUS_ERROR =
             "Review Date must be entered for completed hardship";
-    public static final String NEW_WORK_REASON_VALIDATION_MESSAGE = "Review Reason must be entered for hardship";
-    public static final String SOLICITOR_DETAILS_VALIDATION_MESSAGE =
+    public static final String NEW_WORK_REASON_ERROR = "Review Reason must be entered for hardship";
+    public static final String SOLICITOR_DETAILS_ERROR =
             "Solicitor Number of Hours must be entered when Solicitor Hourly Rate is specified";
-    public static final String EXPENDITURE_OR_DENIED_INCOME_VALIDATION_MESSAGE =
+    public static final String EXPENDITURE_OR_DENIED_INCOME_ERROR =
             "Amount, Frequency, and Reason must be entered for each detail in section ";
-    public static final String PROGRESSION_ITEMS_VALIDATION_MESSAGE =
+    public static final String PROGRESSION_ITEMS_ERROR =
             "Date Taken, Response Required, and Date Required must be entered for each Action Taken in section Review Progress";
 
     private static boolean progressionItemWithoutRequiredDateOrResponseOrDateTaken(HardshipProgress progression) {
@@ -70,7 +70,7 @@ public class HardshipValidationService {
                         StringUtils.isEmpty(deniedIncome.getReasonNote())));
     }
 
-    public void checkHardship(final ApiPerformHardshipRequest apiPerformHardshipRequest) {
+    public void checkHardship(final ApiPerformHardshipRequest apiPerformHardshipRequest, RequestType requestType) {
         validateReviewDate(apiPerformHardshipRequest);
         validateHardshipReviewStatus(apiPerformHardshipRequest);
         validateHardshipReviewNewWorkReason(apiPerformHardshipRequest);
@@ -78,6 +78,17 @@ public class HardshipValidationService {
         validateDeniedIncome(apiPerformHardshipRequest);
         validateExpenditure(apiPerformHardshipRequest);
         validateProgressionItems(apiPerformHardshipRequest);
+        if (requestType == RequestType.UPDATE) {
+            validateUpdate(apiPerformHardshipRequest);
+        }
+    }
+
+    private void validateUpdate(ApiPerformHardshipRequest apiPerformHardshipRequest) {
+        ApiFindHardshipResponse hardship =
+                maatCourtDataService.getHardship(apiPerformHardshipRequest.getHardshipMetadata().getHardshipReviewId());
+        if (hardship.getStatus().equals(HardshipReviewStatus.COMPLETE)) {
+            throw new ValidationException(CANNOT_MODIFY_COMPLETE_HARDSHIP_ERROR);
+        }
     }
 
     public void validateReviewDate(ApiPerformHardshipRequest apiPerformHardshipRequest) {
@@ -89,7 +100,7 @@ public class HardshipValidationService {
         if (isNull(financialAssessment)
                 || financialAssessment.getReplaced().equals("Y")
                 || isNull(financialAssessment.getDateCompleted())) {
-            throw new ValidationException(INCOMPLETE_ASSESSMENT_VALIDATION_MESSAGE);
+            throw new ValidationException(INCOMPLETE_ASSESSMENT_ERROR);
         }
 
         var reviewDate = apiPerformHardshipRequest.getHardship().getReviewDate();
@@ -98,20 +109,20 @@ public class HardshipValidationService {
         var assessmentDate = fullAssessmentDate != null ? fullAssessmentDate : initialAssessmentDate;
 
         if (reviewDate.isBefore(assessmentDate)) {
-            throw new ValidationException(REVIEW_DATE_VALIDATION_MESSAGE);
+            throw new ValidationException(REVIEW_DATE_ERROR);
         }
 
     }
 
     private void validateHardshipReviewStatus(ApiPerformHardshipRequest apiPerformHardshipRequest) {
         if (hardshipStatusIsCompleteWithoutReviewDate(apiPerformHardshipRequest)) {
-            throw new ValidationException(REVIEW_STATUS_VALIDATION_MESSAGE);
+            throw new ValidationException(REVIEW_STATUS_ERROR);
         }
     }
 
     private void validateHardshipReviewNewWorkReason(ApiPerformHardshipRequest apiPerformHardshipRequest) {
         if (isNull(apiPerformHardshipRequest.getHardshipMetadata().getReviewReason())) {
-            throw new ValidationException(NEW_WORK_REASON_VALIDATION_MESSAGE);
+            throw new ValidationException(NEW_WORK_REASON_ERROR);
         }
     }
 
@@ -124,7 +135,7 @@ public class HardshipValidationService {
             solicitorHours = Optional.ofNullable(solicitorCosts.getHours()).orElse(BigDecimal.ZERO);
         }
         if (solicitorRateSpecifiedWithoutSolicitorHours(solicitorRate, solicitorHours)) {
-            throw new ValidationException(SOLICITOR_DETAILS_VALIDATION_MESSAGE);
+            throw new ValidationException(SOLICITOR_DETAILS_ERROR);
         }
     }
 
@@ -133,7 +144,7 @@ public class HardshipValidationService {
         Optional.ofNullable(deniedIncomes).orElse(List.of()).forEach(deniedIncome -> {
             if (deniedIncomeWithoutAmountOrFrequencyOrReasonNote(deniedIncome)) {
                 throw new ValidationException(
-                        EXPENDITURE_OR_DENIED_INCOME_VALIDATION_MESSAGE + deniedIncome.getItemCode().getDescription());
+                        EXPENDITURE_OR_DENIED_INCOME_ERROR + deniedIncome.getItemCode().getDescription());
             }
         });
     }
@@ -143,7 +154,7 @@ public class HardshipValidationService {
         Optional.ofNullable(expenditures).orElse(List.of()).forEach(expenditure -> {
             if (expenditureWithoutAmountOrFrequencyOrReasonCode(expenditure)) {
                 throw new ValidationException(
-                        EXPENDITURE_OR_DENIED_INCOME_VALIDATION_MESSAGE + expenditure.getItemCode().getDescription());
+                        EXPENDITURE_OR_DENIED_INCOME_ERROR + expenditure.getItemCode().getDescription());
             }
         });
     }
@@ -152,7 +163,7 @@ public class HardshipValidationService {
         List<HardshipProgress> progressionItems = apiPerformHardshipRequest.getHardshipMetadata().getProgressItems();
         Optional.ofNullable(progressionItems).orElse(List.of()).forEach(progression -> {
             if (progressionItemWithoutRequiredDateOrResponseOrDateTaken(progression)) {
-                throw new ValidationException(PROGRESSION_ITEMS_VALIDATION_MESSAGE);
+                throw new ValidationException(PROGRESSION_ITEMS_ERROR);
             }
         });
     }
